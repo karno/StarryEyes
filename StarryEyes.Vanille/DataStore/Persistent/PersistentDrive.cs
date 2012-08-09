f8bbea0bce336fff7502e4e14ae3b970861147ac
karno <webmaster@starwing.net>
2012-08-10 03:39:12 +0900

    Improve implementation of PersistentDrive.

--- StarryEyes.Vanille/DataStore/Persistent/PersistentDrive.cs
+++ StarryEyes.Vanille/DataStore/Persistent/PersistentDrive.cs
@@ -26,6 +26,10 @@ namespace StarryEyes.Vanille.DataStore.Persistent
         private SortedDictionary<int, List<int>> nextIndexOfPackets;
         private SortedDictionary<TKey, int> tableOfContents;
 
+        /// <summary>
+        /// Initialize persistent drive with create-new mode.
+        /// </summary>
+        /// <param name="path">base file path</param>
         public PersistentDrive(string path)
         {
             this.tableOfContents = new SortedDictionary<TKey, int>();
@@ -34,6 +38,12 @@ namespace StarryEyes.Vanille.DataStore.Persistent
             PrepareFile(false);
         }
 
+        /// <summary>
+        /// Initialize persistent drive with load previous data mode.
+        /// </summary>
+        /// <param name="path">base file path</param>
+        /// <param name="tableOfContents">previous table of contents</param>
+        /// <param name="nextIndexOfPackets">previous next index of packets table</param>
         public PersistentDrive(string path, IDictionary<TKey, int> tableOfContents, IEnumerable<int> nextIndexOfPackets)
         {
             this.tableOfContents = new SortedDictionary<TKey, int>(tableOfContents);
@@ -45,23 +55,40 @@ namespace StarryEyes.Vanille.DataStore.Persistent
             PrepareFile(true);
         }
 
+        /// <summary>
+        /// Set next index for specified index
+        /// </summary>
+        /// <param name="index">set index</param>
+        /// <param name="next">next index</param>
         private void SetNextIndexOfPackets(int index, int next)
         {
+            // next index of chunks table is delimited by ChunkSize.
+            // every chunks is under controlled by mother tree(nextIndexOfPackets).
             int chunk = index / ChunkSize;
             int key = index % ChunkSize;
             List<int> curChunk;
+
+            // get chunk
             if (this.nextIndexOfPackets.TryGetValue(chunk, out curChunk))
             {
+                // chunk found
                 curChunk[key] = next;
             }
             else
             {
-                var list = new List<int>(Enumerable.Range(Empty, ChunkSize));
+                // chunk not found, create new
+                var list = new List<int>(Enumerable.Range(Empty, ChunkSize)); // init chunk with Empty recode
                 list[key] = next;
+                // add chunk to niop-tree
                 this.nextIndexOfPackets.Add(chunk, list);
             }
         }
 
+        /// <summary>
+        /// Get next index for specified index
+        /// </summary>
+        /// <param name="index">source index</param>
+        /// <returns>next index</returns>
         private int GetNextIndexOfPackets(int index)
         {
             int chunk = index / ChunkSize;
@@ -73,10 +100,15 @@ namespace StarryEyes.Vanille.DataStore.Persistent
             }
             else
             {
+                // pseudo END (data corrupted?)
                 return EndOfIndexOfPackets;
             }
         }
 
+        /// <summary>
+        /// Prepare file for storage
+        /// </summary>
+        /// <param name="isInitializedWithToc">load-mode flag</param>
         private void PrepareFile(bool isInitializedWithToc)
         {
             if (isInitializedWithToc)
@@ -92,31 +124,53 @@ namespace StarryEyes.Vanille.DataStore.Persistent
             }
             else
             {
-                // initialize file
+                // initialize file by empty data.
                 fs = File.Open(path, FileMode.Create, FileAccess.ReadWrite);
             }
         }
 
+        /// <summary>
+        /// Get Table of Contents.
+        /// </summary>
         public IDictionary<TKey, int> GetTableOfContents()
         {
             return tableOfContents;
         }
 
+        /// <summary>
+        /// Store a value.
+        /// </summary>
+        /// <param name="key">storing value's key</param>
+        /// <param name="value">store value</param>
         public void Store(TKey key, TValue value)
         {
+            // serialize data
             var ms = new MemoryStream();
+            ms.Write(new byte[4], 0, 4); // add empty 4 bytes (placeholder)
             using (var bw = new BinaryWriter(ms))
                 value.Serialize(bw);
-            var length = (int)ms.Length;
-            var lengthBytes = BitConverter.GetBytes(length); // byte[4]
-            byte[] writeBuf = new byte[length + 4];
-            Array.Copy(lengthBytes, writeBuf, 4);
-            Array.Copy(ms.ToArray(), 0, writeBuf, 4, ms.Length);
+            var bytes = ms.ToArray();
+
+            // get length-bit into the header
+            var lengthBytes = BitConverter.GetBytes(bytes.Length - 4); // byte[4]
+
+            // paste length bytes into serialize buffer
+            Array.Copy(lengthBytes, bytes, 4);
+
+            // get index for storing data
             int writeTo = GetNextEmptyIndex(0);
             tableOfContents.Add(key, writeTo);
-            StoreInternal(writeBuf, writeTo);
+
+            // store
+            StoreInternal(bytes, writeTo);
         }
 
+        /// <summary>
+        /// Load a value from key.<para />
+        /// If key is not found, throws KeyNotFoundException.
+        /// </summary>
+        /// <param name="key">finding key</param>
+        /// <returns>value</returns>
         public TValue Load(TKey key)
         {
             int readFrom;
@@ -125,6 +179,11 @@ namespace StarryEyes.Vanille.DataStore.Persistent
             return Load(readFrom);
         }
 
+        /// <summary>
+        /// Find values with predicate.
+        /// </summary>
+        /// <param name="predicate">find predicate</param>
+        /// <returns>value sequence</returns>
         public IEnumerable<TValue> Find(Func<TValue, bool> predicate)
         {
             return tableOfContents.Values
@@ -132,6 +191,11 @@ namespace StarryEyes.Vanille.DataStore.Persistent
                 .Where(predicate);
         }
 
+        /// <summary>
+        /// Load from index.
+        /// </summary>
+        /// <param name="index">offset index</param>
+        /// <returns>deserialized value</returns>
         private TValue Load(int index)
         {
             var bytes = LoadInternal(index);
@@ -142,19 +206,31 @@ namespace StarryEyes.Vanille.DataStore.Persistent
             return ret;
         }
 
+        /// <summary>
+        /// Remove from store.
+        /// </summary>
+        /// <param name="key">removing key</param>
+        /// <returns>succeeded or not</returns>
         public bool Remove(TKey key)
         {
             int idx;
             if(!tableOfContents.TryGetValue(key, out idx))
-                return false;
+                return false; // not found
             do
             {
+                // clear all packets table.
                 var newidx = GetNextIndexOfPackets(idx);
                 SetNextIndexOfPackets(idx, Empty);
             } while (idx != 0);
+            // OK
             return true;
         }
 
+        /// <summary>
+        /// Store value.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="startOffset"></param>
         private void StoreInternal(byte[] data, int startOffset)
         {
             // current writing data starting offset
@@ -163,16 +239,19 @@ namespace StarryEyes.Vanille.DataStore.Persistent
             // set offset
             int offset = startOffset;
 
+            // write packets
             while (true)
             {
+                // seek to write destination, write packet
                 fs.Seek(offset * PacketSize, SeekOrigin.Begin);
                 fs.Write(data, cursor, Math.Min(PacketSize, data.Length - cursor));
 
+                // move cursor
                 cursor += PacketSize;
 
                 if (cursor < data.Length)
                 {
-                    SetNextIndexOfPackets(offset, 0);
+                    SetNextIndexOfPackets(offset, 0); // finalize (mark as EOP)
                     break; // write completed
                 }
 
@@ -182,6 +261,11 @@ namespace StarryEyes.Vanille.DataStore.Persistent
             }
         }
 
+        /// <summary>
+        /// Get next empty index of niop.
+        /// </summary>
+        /// <param name="start">find start packet</param>
+        /// <returns>larger than find start packet.</returns>
         private int GetNextEmptyIndex(int start)
         {
             int current = start + 1;
@@ -189,46 +273,45 @@ namespace StarryEyes.Vanille.DataStore.Persistent
             {
                 switch (GetNextIndexOfPackets(current))
                 {
-                    case Empty:
+                    case Empty: // empty block
                         return current;
-                    case EndOfIndexOfPackets:
-                        return nextIndexOfPackets.Count;
+                    case EndOfIndexOfPackets: // EIOP
+                        return nextIndexOfPackets.Count; // next block
                     default:
-                        current++;
+                        current++; // continue loop
                         break;
                 }
             }
         }
 
+        /// <summary>
+        /// Load bytes array.
+        /// </summary>
+        /// <param name="offset">load start offset</param>
+        /// <returns>byte array</returns>
         private byte[] LoadInternal(int offset)
         {
-            // current capacity packets of buffer
-            int capacity = PacketSize;
-            // return data buffer
+            // data reading buffer
             var buffer = new byte[PacketSize];
-            // writing target
-            int cursor = 0;
+            // return data buffer
+            var rstream = new MemoryStream(); 
+
+            // read size
+            int read = 0;
 
             while (true)
             {
+                // seek to source offset, read packet
                 fs.Seek(offset * PacketSize, SeekOrigin.Begin);
-                if (fs.Read(buffer, cursor, PacketSize) != PacketSize) //  corrupted
-                    throw new IOException("Internal File System is corrupted!");
-                cursor += PacketSize;
+                read = fs.Read(buffer, 0, PacketSize);
+
+                // write to return stream
+                rstream.Write(buffer, 0, read);
 
                 // determine next packet index
                 offset = GetNextIndexOfPackets(offset);
                 if (offset == 0)
-                    return buffer;
-
-                // expand buffer
-                if (cursor >= PacketSize)
-                {
-                    capacity *= 2;
-                    var newBuffer = new byte[capacity];
-                    buffer.CopyTo(newBuffer, 0);
-                    buffer = newBuffer;
-                }
+                    return rstream.ToArray(); // return 'return stream' as array
             }
         }
 
