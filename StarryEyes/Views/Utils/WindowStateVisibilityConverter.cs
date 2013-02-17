70213dc34fc1c5d407a0593b1f22aa9357054599
Karno <karnoroid@gmail.com>
2013-02-18 04:20:08 +0900

    Add WindowStateVisibilityConverter.

---
+++ StarryEyes/Views/Utils/WindowStateVisibilityConverter.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+namespace StarryEyes.Views.Utils
+{
+    public class WindowMaximizeToVisibleConverter : OneWayConverter<WindowState, Visibility>
+    {
+        protected override Visibility ToTarget(WindowState input, object parameter)
+        {
+            return input == WindowState.Maximized ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+
+    public class WindowMaximizeToInvisibleConverter : OneWayConverter<WindowState, Visibility>
+    {
+        protected override Visibility ToTarget(WindowState input, object parameter)
+        {
+            return input != WindowState.Maximized ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+
+    public class WindowMinimizeToVisibleConverter : OneWayConverter<WindowState, Visibility>
+    {
+        protected override Visibility ToTarget(WindowState input, object parameter)
+        {
+            return input == WindowState.Minimized ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+
+    public class WindowMinimizeToInvisibleConverter : OneWayConverter<WindowState, Visibility>
+    {
+        protected override Visibility ToTarget(WindowState input, object parameter)
+        {
+            return input != WindowState.Minimized ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
