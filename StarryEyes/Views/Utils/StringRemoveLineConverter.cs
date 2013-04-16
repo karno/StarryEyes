72d1ba8f68bc6150b5c079e886b6da1cad782825
karno <karnoroid@gmail.com>
2013-04-16 22:51:56 +0900

    improve general designs.
    in-reply-to text is now removed line feeds.
    resize handle is now displayed in main window.

---
+++ StarryEyes/Views/Utils/StringRemoveLineConverter.cs
@@ -0,0 +1,11 @@
+﻿
+namespace StarryEyes.Views.Utils
+{
+    public class StringRemoveLineConverter : OneWayConverter<string, string>
+    {
+        protected override string ToTarget(string input, object parameter)
+        {
+            return input.Replace("\r", "").Replace("\n", " ");
+        }
+    }
+}
