f04b2f695c90d40decc42dd6a0a80534afb8a2cd
karno <karnoroid@gmail.com>
2013-08-05 06:30:32 +0900

    Fix reference path in Albireo.

--- StarryEyes.Albireo/Fx/ObservableFx.cs
+++ StarryEyes.Albireo/Fx/ObservableFx.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reactive.Concurrency;
-using System.Reactive.Subjects;
 
 // ReSharper disable CheckNamespace
 namespace System.Reactive.Linq
