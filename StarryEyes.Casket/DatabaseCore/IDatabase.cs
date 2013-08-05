b9d645e6a5c2779e681b0ea9b49ef546d0ef3eee
karno <karnoroid@gmail.com>
2013-08-05 21:31:40 +0900

    add IDatabase interface.

---
+++ StarryEyes.Casket/DatabaseCore/IDatabase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StarryEyes.Casket.DatabaseCore
+{
+    interface IDatabase<T> where T : class
+    {
+        Task<T> Get(long key);
+
+        Task<IEnumerable<T>> Find(IMultiplexPredicate<T> predicate);
+
+        Task Store(T item);
+
+        Task Delete(long key);
+
+        Task Delete(IMultiplexPredicate<T> predicate);
+    }
+}
