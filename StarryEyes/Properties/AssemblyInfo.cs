77ecbf512dd737f9d40658faf24ec2d14d422c86
karno <karnoroid@gmail.com>
2013-10-29 08:06:38 +0900

    Increment revision number.

--- StarryEyes/Properties/AssemblyInfo.cs
+++ StarryEyes/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.8.2.1")]
-[assembly: AssemblyFileVersion("2.8.2.1")]
+[assembly: AssemblyVersion("2.8.2.2")]
+[assembly: AssemblyFileVersion("2.8.2.2")]
