d3d919aa1bb3b8b8286896f5bc64efca5af957b8
Karno <karnoroid@gmail.com>
2013-11-16 21:54:12 +0900

    Fix drag move cause crash.

--- StarryEyes/Views/WindowParts/Flips/SettingFlip.xaml.cs
+++ StarryEyes/Views/WindowParts/Flips/SettingFlip.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace StarryEyes.Views.WindowParts.Flips
 {
@@ -27,8 +17,16 @@ namespace StarryEyes.Views.WindowParts.Flips
 
         protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
         {
-            if(e.GetPosition(null).Y < 48)
-                Window.GetWindow(this).DragMove();
+            var window = Window.GetWindow(this);
+            if (window == null || e.GetPosition(null).Y >= 48) return;
+            try
+            {
+                window.DragMove();
+            }
+            catch (InvalidOperationException)
+            {
+                // failed start drag move.
+            }
         }
     }
 }
