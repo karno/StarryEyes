adb31517e491d8374d5a7a91ba4716b2d2141a12
Karno <karnoroid@gmail.com>
2014-01-26 02:12:07 +0900

    add trace code for checking userstream behavior.

--- StarryEyes.Anomaly/TwitterApi/Streaming/UserStreams.cs
+++ StarryEyes.Anomaly/TwitterApi/Streaming/UserStreams.cs
@@ -26,6 +26,15 @@ namespace StarryEyes.Anomaly.TwitterApi.Streaming
                 {"replies", repliesAll ? "all" : null},
                 {"include_followings_activity", followingsActivity ? "true" : null}
             }.ParametalizeForGet();
+
+            var dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "SEUTRACE");
+            Directory.CreateDirectory(dir);
+            var logFile = Path.Combine(dir, credential.OAuthAccessToken + ".log");
+
+            var fstream = File.Open(logFile, FileMode.OpenOrCreate, FileAccess.Write);
+            var writer = new StreamWriter(fstream);
+
+            writer.WriteLine(DateTime.Now.ToString() + ": #CONOPEN");
             return Observable.Create<string>((observer, cancel) => Task.Run(async () =>
             {
                 try
@@ -49,6 +58,7 @@ namespace StarryEyes.Anomaly.TwitterApi.Streaming
                             var delay = Task.Delay(TimeSpan.FromSeconds(ApiAccessProperties.StreamingTimeoutSec), cancel);
                             if (await Task.WhenAny(readLine, delay) == delay)
                             {
+                                writer.WriteLine(DateTime.Now.ToString() + ": #TIMEOUT");
                                 // timeout
                                 System.Diagnostics.Debug.WriteLine("#USERSTREAM# TIMEOUT.");
                                 break;
@@ -56,23 +66,41 @@ namespace StarryEyes.Anomaly.TwitterApi.Streaming
                             var line = readLine.Result;
                             if (line == null)
                             {
+                                writer.WriteLine(DateTime.Now.ToString() + ": #CONCLOSE");
                                 // connection closed
                                 System.Diagnostics.Debug.WriteLine("#USERSTREAM# CONNECTION CLOSED.");
                                 break;
+                            }
+                            if (String.IsNullOrEmpty(line))
+                            {
+                                writer.WriteLine(DateTime.Now.ToString() + ": RECEIVED EMPTY LINE");
+                            }
+                            else
+                            {
+                                writer.WriteLine(DateTime.Now.ToString() + ": RECEIVED DIGEST" + line.Substring(0, Math.Min(10, line.Length)));
                             }
                             if (!String.IsNullOrEmpty(line))
                             {
                                 // successfully completed
                                 observer.OnNext(line);
                             }
+                            writer.Flush();
                         }
+                        writer.WriteLine(DateTime.Now.ToString() + ": EXIT LOOP.");
+                        writer.WriteLine(DateTime.Now.ToString() + ": CREQ?:" + cancel.IsCancellationRequested);
+                        writer.Flush();
                     }
                 }
                 catch (Exception ex)
                 {
+                    writer.WriteLine(DateTime.Now.ToString() + ": THROWN:" + ex.Message);
                     System.Diagnostics.Debug.WriteLine("#USERSTREAM# error detected: " + ex.Message);
                     observer.OnError(ex);
                     return;
+                }
+                finally
+                {
+                    writer.Dispose();
                 }
 
                 System.Diagnostics.Debug.WriteLine("#USERSTREAM# disconnection detected. (CANCELLATION REQUEST? " + cancel.IsCancellationRequested + ")");
