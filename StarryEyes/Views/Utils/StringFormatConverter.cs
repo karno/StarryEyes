2f63eaa9b1f7d9ef339e5e900515bb08b9e9d9a2
Karno <karnoroid@gmail.com>
2014-01-30 10:58:19 +0900

    Theme system was fully implemented. (but some works about themes still remained.)

---
+++ StarryEyes/Views/Utils/StringFormatConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StarryEyes.Views.Utils
+{
+    public class StringFormatConverter : OneWayConverter<object, string>
+    {
+        protected override string ToTarget(object input, object parameter)
+        {
+            return String.Format(parameter as string ?? "{0}", input);
+        }
+    }
+}
