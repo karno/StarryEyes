6229a6454ae1683b6e047e44030053b78250bb51
Karno <karnoroid@gmail.com>
2014-10-16 22:45:44 +0900

    Fix: jumping scroll to top when pixel-base setting is disabled (#139).

--- StarryEyes/Views/Behaviors/TimelineScrollHandlerBehavior.cs
+++ StarryEyes/Views/Behaviors/TimelineScrollHandlerBehavior.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows.Controls;
 using System.Windows.Interactivity;
+using StarryEyes.Settings;
 
 namespace StarryEyes.Views.Behaviors
 {
@@ -13,13 +14,15 @@ namespace StarryEyes.Views.Behaviors
 
         void AssociatedObject_PreviewMouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
         {
+            // this bugfix is available only on scroll by pixel setting.
+            if (!Setting.IsScrollByPixel.Value) return;
+
             var newOffset = this.AssociatedObject.VerticalOffset - e.Delta;
-            if (newOffset < 1)
-            {
-                // capture scroll event (for preventing freeze)
-                this.AssociatedObject.ScrollToTop();
-                e.Handled = true;
-            }
+            if (newOffset >= 1) return;
+
+            // capture scroll event (for preventing freeze)
+            this.AssociatedObject.ScrollToTop();
+            e.Handled = true;
         }
 
         protected override void OnDetaching()
