dcf159299a3ecb0443719eb88d07f81b68d1792d
Karno <karnoroid@gmail.com>
2015-02-04 10:24:22 +0900

    defining interfaces...

---
+++ StarryEyes.Illumine/Settings/ITwitterAccount.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using JetBrains.Annotations;
+using StarryEyes.Anomaly;
+using StarryEyes.Anomaly.TwitterApi;
+
+namespace StarryEyes.Illumine.Settings
+{
+    public interface ITwitterAccount : IOAuthCredential
+    {
+        /// <summary>
+        /// <para>Fallback target ID</para>
+        /// <para>If specified ID is not found in setting store, ignore it.</para>
+        /// </summary>
+        long? FallbackAccountId { get; set; }
+
+        /// <summary>
+        /// <para>Fallback favorites just like do with tweets.</para>
+        /// </summary>
+        bool IsFallbackFavorite { get; set; }
+
+        /// <summary>
+        /// Caching objects
+        /// </summary>
+        [NotNull]
+        ITwitterAccountCache Cache { get; }
+
+        /// <summary>
+        /// Properties for UserStreams
+        /// </summary>
+        [NotNull]
+        IUserStreamsProperties UserStreamsProperties { get; }
+
+        /// <summary>
+        /// Properties for accessing twitter
+        /// </summary>
+        [NotNull]
+        IApiAccessProperties ApiAccessProperties { get; }
+    }
+
+    public interface ITwitterPostingProperties
+    {
+        /// <summary>
+        /// <para>Mark uploaded medias as sensitive contents.</para>
+        /// <para>(if true, set flag possibly_sensitive=true)</para>
+        /// </summary>
+        bool MarkMediaAsPossiblySensitive { get; set; }
+    }
+
+    public interface ITwitterAccountCache
+    {
+        /// <summary>
+        /// Cached screen name
+        /// </summary>
+        string ScreenName { get; set; }
+
+        /// <summary>
+        /// Cached user name
+        /// </summary>
+        string UserName { get; set; }
+
+        /// <summary>
+        /// Cached profile image URI
+        /// </summary>
+        Uri ProfileImageUri { get; set; }
+    }
+
+    public interface IUserStreamsProperties
+    {
+        /// <summary>
+        /// Use user streams for receiving tweets.
+        /// </summary>
+        bool Enabled { get; set; }
+
+        /// <summary>
+        /// <para>Receive replies to following users.</para>
+        /// <para>(if true, set flag replies=all)</para>
+        /// </summary>
+        bool ReceiveRepliesToFollowings { get; set; }
+
+        /// <summary>
+        /// <para>Receive activities caused by following users.</para>
+        /// <para>(if true, set flag include_followings_activity=true)</para>
+        /// </summary>
+        bool ReceiveActivitiesFromFollowings { get; set; }
+    }
+
+
+}
---
+++ StarryEyes.Illumine/Settings/Setting.cs
@@ -0,0 +1,25 @@
+﻿
+namespace StarryEyes.Illumine.Settings
+{
+    public class Setting
+    {
+        private const string DefaultFileName = "krile_i.xml";
+
+        private static Setting _defaultSetting;
+
+        public static Setting Default
+        {
+            get { return _defaultSetting ?? (_defaultSetting = Load(DefaultFileName, false)); }
+        }
+
+        public static Setting Load(string file, bool ignoreBackup)
+        {
+
+        }
+
+        public void Save()
+        {
+
+        }
+    }
+}
